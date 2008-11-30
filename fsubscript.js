b4b60ba02dfc6a2e1a37d7623f3a076700173e4c
ecaradec <emmanuel.caradec@gmail.com>
2008-11-30 18:18:48 +0100

    finalize first version

--- fsubscript.js
+++ fsubscript.js
@@ -1,31 +1,11 @@
 (function() {
-    function dump(arr,level) {
-        var dumped_text = "";
-        if(!level) level = 0;
-        
-        //The padding given at the beginning of the line.
-        var level_padding = "";
-        for(var j=0;j<level+1;j++) level_padding += "    ";
-        
-        if(typeof(arr) == 'object') { //Array/Hashes/Objects 
-            for(var item in arr) {
-                var value = arr[item];
-                
-                if(typeof(value) == 'object') { //If it is an array,
-                    dumped_text += level_padding + "'" + item + "' ...\n";
-                    dumped_text += dump(value,level+1);
-                } else {
-                    dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-                }
-            }
-        } else { //Stings/Chars/Numbers etc.
-            dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
-        }
-        return dumped_text;
-    }
-
-    function p(txt) {
-        WScript.Echo(txt);
+    function doDeferedSearch(txt) {    
+        FARR.setInterval(0, 1,function() { 
+            FARR.killInterval(0);
+            FARR.setStrValue("stopsearch","");
+            FARR.setStrValue("launch","dosearch "+txt);
+            //displayBalloonMessage(txt);
+        });
     }
 
     function scan(r,o,v,opt) {
@@ -39,81 +19,125 @@
     }
 
     var filerx=/^([a-zA-Z]:.*?)(;|$| to )/
-    function parseExpr() {
-        r.push(scan(/^fssc \+(.*?) /,0,1));
-        r.push([]);
-        r[1].push(scan(filerx,1,1));
-        while(scan(/^;/,0,0,true)) {
-            r[1].push(scan(filerx,1,1))
+    function parseExpr(query) {
+        r=[];
+        q=query;
+        index=0;
+
+        try {            
+            r.push({
+                type:"alias",
+                value:scan(/^(.+?) /,0,1),
+                toString:function() { return this.value+" "; }
+            });
+            r.push({
+                type:"filelist",
+                value:[scan(filerx,1,1)],
+                toString:function() { return this.value.join(""); }
+            });
+
+            while(scan(/^;/,0,0,true)) {
+                r[1].value.push(";")
+                r[1].value.push(scan(filerx,1,1))
+            }
+            r.push({
+                type:"modifier",
+                value:scan(/^ to /,0,0),
+                toString:function() { return this.value; }
+            });
+            r.push({
+                type:"file",
+                value:scan(filerx,1,1),
+                toString:function() { return this.value; }
+            });
+        } catch(e) {
+            //displayBalloonMessage(e);
         }
-        r.push(scan(/^ to /,0,0));
-        r.push(scan(filerx,1,1));
         return r;
+    }
+    function toQuery() {
+        var z="";
+        for(var i=0;i<r.length;i++)
+            z+=r[i].toString();
+
+        return z;
+    }
+    function execSearch() {
+        if(r.length==2 && r[0].type=="alias" && r[0].value=="zip" && r[1].type=="filelist")
+            doDeferedSearch(r[1].value[r[1].value.length-1]);
+        else if(r.length==4 && r[0].type=="alias" && r[0].value=="zip" && r[1].type=="filelist" && r[2].type=="modifier" && r[2].value==" to " && r[3].type=="file")
+            doDeferedSearch(r[3].value+" +invoke");
+    }
+    function execComplete(path) {
+        if(r.length==2 && r[0].type=="alias" && r[0].value=="zip" && r[1].type=="filelist") {
+            r[1].value[r[1].value.length-1]=path;
+            return toQuery();
+        } else if(r.length==4 && r[0].type=="alias" && r[0].value=="zip" && r[1].type=="filelist" && r[2].type=="modifier" && r[2].value==" to " && r[3].type=="file") {
+            r[3].value=path;
+            return toQuery();
+        }
+    }
+    function execInvoke(path) {
+        //displayBalloonMessage("invoke : "+execComplete(path));
+        execComplete(path);        
+
+        var cmdLine="a \""+r[3].value+"\" -r ";//+r[1].value.join("\" ").replace(/;/g,"")+"\"";
+        for(var i=0;i<r[1].value.length;i++) {
+            if(r[1].value[i]==";")
+                continue;
+            if(r[1].value[i].match(/\\$/))
+                cmdLine+=" \""+r[1].value[i]+"*.*\"";
+            else
+                cmdLine+=" \""+r[1].value[i]+"\"";
+        }
+
+        //displayBalloonMessage(cmdLine);
+        FARR.exec(directory+"\\7z.exe", cmdLine, "");
     }
 
     var r;
     var q;
     var index;
-    //var r=[];
-    //var q="fssc +zip c:\\toto;c:\\tutu to c:\\tata";
-    //var index=0;
-    //try {
-    //    parseExpr();
-    //} catch(e) { p("partial match"); }
+    var directory=currentDirectory;
 
-
-    plugins["zip"]={
-        version:"v1.0.0",
-        lastChange:"Nov 13st 2008",
+    plugins["FZip"]={
+        aliasstr:"zip",
+        version:"v0.1.0",
+        lastChange:"Nov 30st 2008",
         displayName:"Zipper",
         directory:currentDirectory,
         icon:currentDirectory+"\\zipper.ico",
         //autocomplete:"colors", // replace the default completion : you need to define an alias file to use it
-        search:function(querykey, explicit, queryraw, querynokeyword, modifier, triggermethod) {
-            if(!explicit) return;        
-
-            r=[];
-            q=queryraw;
-            index=0;
-
-            try {
-                parseExpr();
-            } catch(e) { /*p("partial match");*/ }
-
-            displayBalloonMessage(dump(r));
-
-            if(r.length==2 && r[0]=="zip") {
-                //displayBalloonMessage(dump(r));
-                doDeferedSearch(r[1][0])
+        search:function(querykey, explicit, queryraw, modifier, triggermethod) {
+            if(queryraw.indexOf("+invoke")!=-1) {       
+                var file=queryraw.replace("+invoke","")
+                FARR.emitResult(querykey,FARR.getQueryString(), file, this.icon,UNKNOWN,IMMEDIATE_DISPLAY,-1);
             }
 
-            /*var q=queryraw;
-            var p=Math.max(q.lastIndexOf(";"), q.lastIndexOf("fssc +zip ")+9);
+            if(!explicit) return;
 
-            if(p!=-1) {
-                displayBalloonMessage(queryraw.substring(p+1));
-                var z=queryraw.substring(p+1)
-                if(z!="")
-                    doDeferedSearch(z);
-            }else {
-                setRichEditMode("fssc +zip <File>;<File>;<File> to <TargetFile>");
-            }*/
+            FARR.setStrValue("ForceResultFilter", queryraw.substring(4));
+            var r=parseExpr(queryraw.replace(/\//g,"\\"));
+            execSearch(querykey);
         },
         trigger:function(path, title, groupname, pluginid, thispluginid, score, entrytype, args) {
             if(path.indexOf("dosearch")==0)
                 return;
 
-            /*if(FARR.getQueryString().indexOf("fssc +zip")!=0 || entrytype!=FILE && entrytype!=FOLDER)
-                return;
-            
-            var q=FARR.getQueryString();
+            var r=parseExpr(FARR.getQueryString());
 
-            var p=Math.max(q.lastIndexOf(";"), q.lastIndexOf("fssc +zip ")+9);
-            var z=q.substring(0, p+1);
-            if(z!="")
-                FARR.setStrValue("setsearchnogo",z+path);*/
-            
-            //return HANDLED;
+            if(r.length==0 || r[0].type!="alias" || r[0].value!="zip")
+                return;
+
+            if(entrytype==FOLDER)
+                path+="\\";
+
+            var hasCtrl=(FARR.getKeyState(0x11)&0x8000)!=0;
+            if(hasCtrl)
+                restartSearch(execComplete(path));            
+            else
+                execInvoke(path);
+            return HANDLED;
         },
         showSettings:function() {
             //FARR.debug("showSettings");
